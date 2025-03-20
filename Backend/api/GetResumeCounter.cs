bb02026ac9f21c773d0cc5c03214dfc7955911d8
Angel Garcia Roman <angel@MacBook-Air-de-Angel.local>
2025-03-20 12:02:00 +1000

    Api function updated

--- Backend/api/GetResumeCounter.cs
+++ Backend/api/GetResumeCounter.cs
@@ -3,32 +3,40 @@ using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.CosmosDB;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using Microsoft.Azure.Cosmos;
+
 
 namespace Company.Function
 {
     public static class GetResumeCounter
     {
         [FunctionName("GetResumeCounter")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        public static HttpResponseMessage Run(
+                [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+                [CosmosDB(databaseName: "agazureresume", containerName: "counter", Connection = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] Counter counter,
+                [CosmosDB(databaseName: "agazureresume", containerName: "counter", Connection = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] out Counter updatedCounter,
+                ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            updatedCounter = counter;
+            updatedCounter.Count += 1;
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            var jsonToRetun = JsonConvert.SerializeObject(counter);
 
-            return new OkObjectResult(responseMessage);
+            return new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+            {
+                Content = new StringContent(jsonToRetun, Encoding.UTF8, "application/json")
+            };
+
         }
     }
+
 }
